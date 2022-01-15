dd23ae21181c599188d13c860b4ce2c5af7f6e8b
coastss <55933271+coastss@users.noreply.github.com>
2022-01-15 18:51:55 -0500

    Update loader.lua

--- utilities/loader.lua
+++ utilities/loader.lua
@@ -23,13 +23,10 @@ local Loader = Library:Create("ScreenGui", {
     Enabled = false
 })
 
-if syn and syn.protect_gui then
+if syn.protect_gui then
     syn.protect_gui(Loader)
 elseif get_hidden_gui then
     get_hidden_gui(Loader)
-else
-    shared:Kick("[ui_library.lua]: protect_gui function not found.")
-    return
 end
 
 if gethui then
